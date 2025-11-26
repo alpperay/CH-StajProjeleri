fa5b23d5156f8405a635ac17e56320d43a03e3c3
Alp Taşçı <alptascix@gmail.com>
2025-11-26 16:30:28 +0300

    KisiYonetimSistemi_SQLServer projesi eklendi.

---
+++ KisiYonetimSistemi-SQLServer/KisiYonetimSistemi-SQLServer/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace KisiYonetimSistemi_SQLServer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.dgvKisiler = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKisiler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.BackColor = System.Drawing.Color.Silver;
+            this.lblAd.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAd.Location = new System.Drawing.Point(42, 56);
+            this.lblAd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(28, 19);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Ad";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.BackColor = System.Drawing.Color.Silver;
+            this.lblSoyad.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSoyad.Location = new System.Drawing.Point(42, 122);
+            this.lblSoyad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(56, 19);
+            this.lblSoyad.TabIndex = 0;
+            this.lblSoyad.Text = "Soyad";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.BackColor = System.Drawing.Color.Silver;
+            this.lblTelefon.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTelefon.Location = new System.Drawing.Point(42, 194);
+            this.lblTelefon.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(63, 19);
+            this.lblTelefon.TabIndex = 0;
+            this.lblTelefon.Text = "Telefon";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Silver;
+            this.lblEmail.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEmail.Location = new System.Drawing.Point(42, 270);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(48, 19);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(46, 80);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(188, 26);
+            this.txtAd.TabIndex = 2;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(46, 146);
+            this.txtSoyad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(188, 26);
+            this.txtSoyad.TabIndex = 2;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(46, 218);
+            this.txtTelefon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(188, 26);
+            this.txtTelefon.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(46, 294);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(188, 26);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // dgvKisiler
+            // 
+            this.dgvKisiler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKisiler.Location = new System.Drawing.Point(260, 80);
+            this.dgvKisiler.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvKisiler.Name = "dgvKisiler";
+            this.dgvKisiler.Size = new System.Drawing.Size(617, 249);
+            this.dgvKisiler.TabIndex = 3;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnEkle.Font = new System.Drawing.Font("Myanmar Text", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEkle.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnEkle.Location = new System.Drawing.Point(46, 358);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(118, 32);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSil.Font = new System.Drawing.Font("Myanmar Text", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnSil.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnSil.Location = new System.Drawing.Point(172, 358);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(118, 32);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnGuncelle.Font = new System.Drawing.Font("Myanmar Text", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnGuncelle.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnGuncelle.Location = new System.Drawing.Point(298, 358);
+            this.btnGuncelle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(118, 32);
+            this.btnGuncelle.TabIndex = 6;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = false;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(570, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Veri Ara..";
+            // 
+            // txtArama
+            // 
+            this.txtArama.Location = new System.Drawing.Point(653, 359);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(117, 26);
+            this.txtArama.TabIndex = 8;
+            this.txtArama.TextChanged += new System.EventHandler(this.txtArama_TextChanged);
+            // 
+            // btnAra
+            // 
+            this.btnAra.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAra.Font = new System.Drawing.Font("Myanmar Text", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnAra.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnAra.Location = new System.Drawing.Point(776, 358);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(114, 30);
+            this.btnAra.TabIndex = 9;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(902, 474);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.txtArama);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dgvKisiler);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.lblAd);
+            this.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "KisiYonetimSistemi - SQL Server";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKisiler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.DataGridView dgvKisiler;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.Button btnAra;
+    }
+}
