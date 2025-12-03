505946d86270cabeb4ebc5938a30faa73ca0dd1f
Alp Taşçı <alptascix@gmail.com>
2025-12-03 10:51:04 +0300

    KisiYonetimSistemi-XML projesi eklendi.

---
+++ KisiYonetimSistemi/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace KisiYonetimSistemi
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.dgvKisiler = new System.Windows.Forms.DataGridView();
+            this.btnKaydetXml = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKisiler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(25, 25);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(32, 19);
+            this.lblAd.TabIndex = 100;
+            this.lblAd.Text = "Ad:";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(25, 76);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(55, 19);
+            this.lblSoyad.TabIndex = 101;
+            this.lblSoyad.Text = "Soyad:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(25, 127);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(49, 19);
+            this.lblEmail.TabIndex = 102;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(25, 178);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(62, 19);
+            this.lblTelefon.TabIndex = 103;
+            this.lblTelefon.Text = "Telefon:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(25, 47);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(200, 26);
+            this.txtAd.TabIndex = 0;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(25, 98);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(200, 26);
+            this.txtSoyad.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(25, 149);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 26);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(25, 200);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(200, 26);
+            this.txtTelefon.TabIndex = 3;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(250, 47);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(200, 26);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(250, 98);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(200, 26);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(250, 149);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(200, 26);
+            this.btnGuncelle.TabIndex = 6;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // dgvKisiler
+            // 
+            this.dgvKisiler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKisiler.Location = new System.Drawing.Point(12, 248);
+            this.dgvKisiler.Name = "dgvKisiler";
+            this.dgvKisiler.RowHeadersWidth = 51;
+            this.dgvKisiler.RowTemplate.Height = 24;
+            this.dgvKisiler.Size = new System.Drawing.Size(598, 250);
+            this.dgvKisiler.TabIndex = 7;
+            this.dgvKisiler.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvKisiler_CellClick);
+            // 
+            // btnKaydetXml
+            // 
+            this.btnKaydetXml.Location = new System.Drawing.Point(250, 200);
+            this.btnKaydetXml.Name = "btnKaydetXml";
+            this.btnKaydetXml.Size = new System.Drawing.Size(200, 26);
+            this.btnKaydetXml.TabIndex = 104;
+            this.btnKaydetXml.Text = "XML Kaydet";
+            this.btnKaydetXml.UseVisualStyleBackColor = true;
+            this.btnKaydetXml.Click += new System.EventHandler(this.btnKaydetXml_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(793, 520);
+            this.Controls.Add(this.btnKaydetXml);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.dgvKisiler);
+            this.Font = new System.Drawing.Font("Nirmala UI", 10.25F, System.Drawing.FontStyle.Bold);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Kişi Yönetim Sistemi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKisiler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblTelefon;
+
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefon;
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+
+        private System.Windows.Forms.DataGridView dgvKisiler;
+        private System.Windows.Forms.Button btnKaydetXml;
+    }
+}
